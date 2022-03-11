4c64036cea376fc4a467984737b7e21942139d49
Kimzoo <kimzoo1207@gmail.com>
2022-03-12 04:09:29 +0900

    kimzoo 1) 메뉴 각 js로 호출 함수로 변경 (로그아웃 동일 적용)

--- whale/src/main/java/com/dev/whale/controller/QnaController.java
+++ whale/src/main/java/com/dev/whale/controller/QnaController.java
@@ -2,18 +2,14 @@ package com.dev.whale.controller;
 
 import com.dev.whale.domain.model.Category;
 import com.dev.whale.domain.model.Qna;
-import com.dev.whale.domain.model.User;
 import com.dev.whale.service.QnaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -28,11 +24,11 @@ public class QnaController {
     }
 
     @GetMapping("/qnaListView")
-    public String qnaListView(Model model){
+    public String qnaListView(Model model, @RequestParam String username){
 
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(username);
 
-        List<Qna> qnaList = QnaService.selectMyQnaList(auth.getName());
+        List<Qna> qnaList = QnaService.selectMyQnaList(username);
 
         if (qnaList.size() > 0) {
             model.addAttribute("qnaList", qnaList);
--- whale/src/main/resources/static/js/common/common.js
+++ whale/src/main/resources/static/js/common/common.js
@@ -1,7 +1,38 @@
+/* nav */
+let username = $("#username_nav").text();
+
 $("#menu_btn").on("click", function() {
     $("#nav").animate({width:'toggle'},350);
 });
 
 $("#btn-close").on("click", function() {
     $("#nav").css("display", "none");
+});
+
+/* nav a click event */
+$("#myInfo_nav").click(function(){
+    location.href="/myInfo/myForm?username=" +username;
+});
+
+$("#myPost_nav").click(function(){
+    location.href="/post/myPostList?username=" +username;
+});
+
+$("#qna_nav").click(function(){
+    location.href="/qna/qnaListView?username=" +username;
+});
+
+$("#logout_nav").click(function(){
+
+    $.ajax({
+        type: "POST",
+        url: "/account/logout",
+        success: function () {
+            window.location.href = "/";
+        },
+        error: function () {
+
+        }
+    });
+
 });
